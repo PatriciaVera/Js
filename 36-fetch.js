2a82a24ae1f8e581c27848f7c62231c008e04fcc
Patricia Vera <ferpat2218@gmail.com>
2019-07-04 20:03:01 -0500

    Mostrar listado de usuarios y usuario unico

--- 36-fetch.js
+++ 36-fetch.js
@@ -2,14 +2,48 @@
 
 //Fetch y peticiones a servicios apis rest
 
-fetch('https://reqres.in/api/users')
-    .then(data => data.json()) //capturar datos funciòn de fecha convierte datos a json y hace return
+var div_usuarios = document.querySelector("#usuarios");
+var div_janet = document.querySelector("janet");
+
+    getUsuarios()
+    .then(data => data.json())
     .then(users => {
-        usuarios = users.data; //guardar datos
-        console.log(usuarios);
-        
-        usuarios.map((users, i) => {
+        listadoUsuarios(users.data);
+
+        return getJanet();
+    })
+    .then(data => data.json())
+    .then(user => {
+        mostrarJanet(user.data);
+    });
+
+    function getUsuarios(){
+        fetch('https://reqres.in/api/users'); //promesa
+    }
+
+    
+    function getJanet(){
+        fetch('https://reqres.in/api/users/2'); //promesa
+    }
+
+    function listadoUsuarios(usuarios){
+        usuarios.map((user, i) => {
             let nombre = document.createElement('h2');
             
-        })
-    })
+            nombre.innerHTML = i + ". " + user.first_name + " " + user.last_name;
+            div_usuarios.appendChild(nombre);
+
+            document.querySelector(".loading").style.display = "none";
+        });
+    }
+
+    function mostrarJanet(user){
+            let nombre = document.createElement('h4');
+            
+            nombre.innerHTML =  ". " + user.first_name + " " + user.last_name;
+            div_janet.appendChild(nombre);
+
+            document.querySelector("#janet .loading").style.display = "none";
+        }
+
+    
